9bd5d66236bd94f4a69524a6e12cf87edb139749
Joyo Nurdiansyah <nurdiansyahjoyo@gmail.com>
2024-06-03 09:42:58 +0700

    tambahkan avatar

--- app/Http/Controllers/SocialController.php
+++ app/Http/Controllers/SocialController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Laravel\Socialite\Facades\Socialite;
+use Illuminate\Support\Facades\Session;
 
 class SocialController extends Controller
 {
@@ -15,8 +17,21 @@ class SocialController extends Controller
     public function googleCallback()
     {
         $user = Socialite::driver('google')->user();
-        return view('pages.home', [
-            'user' => $user
-        ]);
+        $avatar = $user->getAvatar();
+    
+        $user = \App\Models\User::where('email', $user->email)->first();
+    
+        if ($user) {
+            Auth::login($user);
+            Session::put('avatar', $avatar);
+            return view('pages.home', [
+                'user' => $user,
+                'avatar' => $avatar,
+            ]);
+        } else {
+            return view('not-found.user-not-found', [
+                'user' => null
+            ]);
+        }
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,19 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SocialController;
 
-Route::get('/auth/redirect', [SocialController::class, 'redirect'])->name('google.redirect');
-Route::get('/auth/google/callback', [SocialController::class, 'googleCallback'])->name('google.callback');
+Route::middleware(['web'])->group(function () {
+    Route::get('/auth/redirect', [SocialController::class, 'redirect'])->name('google.redirect');
+    Route::get('/auth/google/callback', [SocialController::class, 'googleCallback'])->name('google.callback');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+    Route::get('/', function () {
+        return view('welcome');
+    });
 
-Route::get('/home-page', function () {
-    return view('pages.home');
+    Route::get('/home-page', function () {
+        return view('pages.home');
+    });
+
+    Route::get('/user-not-found', function () {
+        return view('not-found.user-not-found');
+    });
 });
