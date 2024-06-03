7c0256bdfbf521086ad2699171b126470e6fdfef
Joyo Nurdiansyah <nurdiansyahjoyo@gmail.com>
2024-06-03 08:25:23 +0700

    add section login with google

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SocialController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/auth/redirect', [SocialController::class, 'redirect'])->name('google.redirect');
+Route::get('/auth/google/callback', [SocialController::class, 'googleCallback'])->name('google.callback');
 
 Route::get('/', function () {
     return view('welcome');
